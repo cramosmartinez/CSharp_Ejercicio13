80941bbcedacf7e75e042f4dad9eb5ffa7b0ebed
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-07-21 18:29:15 -0600

    Agregar archivos de proyecto.

---
+++ CSharp_Ejercicio13/Program.cs
@@ -0,0 +1,20 @@
+﻿//Comprobar si un numero introducido por consola es primo
+Console.WriteLine("Ingrese un número:");
+int numero125 = Convert.ToInt32(Console.ReadLine());
+bool esPrimo = true;
+for (int i = 2; i < numero125; i++)
+{
+    if (numero125 % i == 0)
+    {
+        esPrimo = false;
+        break;
+    }
+}
+if (esPrimo)
+{
+    Console.WriteLine("El número " + numero125 + " es primo.");
+}
+else
+{
+    Console.WriteLine("El número " + numero125 + " no es primo.");
+}
